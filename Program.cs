9c9beea9d576e068d5ed6e83340e2f0e3efa6ed5
ThinkPad T14 <ThinkPad T14@DESKTOP-C9FJOHD>
2024-07-02 13:48:42 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*// Numerik
+        int umur = 16;
+        float tinggi = 170.5f;
+        Console.WriteLine("Umur: " + umur);
+        Console.WriteLine("Tinggi: " + tinggi);
+
+        // String
+        string nama = "Budi";
+        Console.WriteLine("Nama: " + nama);
+
+        // Tanggal
+        DateTime hariIni = (DateTime.Now).AddDays(2);
+        Console.WriteLine("Hari ini: " + hariIni.ToString("dd/MM/yyyy"));
+
+        // Boolean
+        bool isLulus = true;
+        Console.WriteLine("Apakah lulus? " + isLulus);*/
+
+     
+    }
+}
+
